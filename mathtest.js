28776ca5dea5299e61c01b5cc24e7e9c65d8301b
Dhruv Tripathi <dhruv.tr03@gmail.com>
2024-05-16 14:35:41 -0500

    code testing

---
+++ mathtest.js
@@ -0,0 +1,29 @@
+//Task 1
+
+let var1 = 10, var2 = 20, var3 = 30, var4 = 40;
+
+let result1 = var1 + var2;
+
+let result3 = var4 - var3;
+
+let finalResult = result1 * result3;
+
+let evenOddResult = finalResult % 2;
+
+console.log(evenOddResult);
+
+//Task 2
+
+let result = 7 + 13 / 9 + 7;
+let result2 = 100 / 2 * 6;
+
+result *= result2;
+
+finalResult = result.toFixed(2);
+
+finalNumber = Number(finalResult);
+
+console.log(finalNumber);
+
+//Task 3
+
