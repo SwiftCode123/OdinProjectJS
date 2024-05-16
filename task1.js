41f1d9190f08a9b09242eb19bc7c3b8b78bcad97
Dhruv Tripathi <dhruv.tr03@gmail.com>
2024-05-16 12:19:53 -0500

    variables and operators

---
+++ task1.js
@@ -0,0 +1,19 @@
+//Task 1
+
+let admin;
+let userName;
+userName = 'John';
+
+admin = userName;
+
+alert(admin);
+
+//Task 2
+
+let earth = "Our planet earth";
+let currentVisitor = "True";
+
+//Task 3
+const BIRTHDAY = '18.04.1982'; //Made birthday uppercase
+
+const age = someCode(birthday); //age isn't really a constant
