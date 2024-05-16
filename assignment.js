92a8d5806f42f5f42d5e808a5be8cc9e5b481638
Dhruv Tripathi <dhruv.tr03@gmail.com>
2024-05-16 14:35:26 -0500

    js learning, code testing

---
+++ assignment.js
@@ -0,0 +1,27 @@
+//#1
+
+console.log(23 + 97);
+
+//#2
+
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+//#3
+
+let result1 = ((4 + 6 + 9) / 77).toFixed(5);
+console.log(result1);
+
+//#4
+
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+//#5
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage);
--- task1.js
+++ task1.js
@@ -6,7 +6,7 @@ userName = 'John';
 
 admin = userName;
 
-alert(admin);
+// alert(admin);
 
 //Task 2
 
@@ -16,4 +16,47 @@ let currentVisitor = "True";
 //Task 3
 const BIRTHDAY = '18.04.1982'; //Made birthday uppercase
 
-const age = someCode(birthday); //age isn't really a constant
+// const age = someCode(birthday); //age isn't really a constant
+
+let x = 123e5;
+
+console.log(x);
+
+let x2 = "100";
+let y = "10";
+let z = x2 / y;
+
+console.log(z);
+
+let x3 = 0xFF;
+console.log(x3);
+
+//Hexadecimal is base 16. Decimal is base 10. Octal is base 8. Binary is base 2.
+// let num = 32;
+// alert(num.toString(36));
+
+let x4 = 123;
+let y2 = new Number(123);
+
+const myInt = 5;
+const myFloat = 6.667;
+
+myInt;
+myFloat;
+
+console.log(typeof myInt);
+console.log(typeof myFloat);
+
+const lotsOfDecimal = 1.766584958675746364;
+lotsOfDecimal;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+
+console.log(twoDecimalPlaces);
+
+let myNumber = "74";
+myNumber = Number(myNumber);
+myNumber += 3;
+console.log(myNumber);
+
+let num1 = 4;
+console.log(num1++);
